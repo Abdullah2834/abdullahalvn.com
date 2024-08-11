d27e3baa3845bb4fa2ab4b73b1d54dc1517eb469
Abdullah <abdullah@gulenayva.com>
2024-08-12 00:18:46 +0300

    added banner

--- myapp/src/components/layout/header/header.svelte
+++ myapp/src/components/layout/header/header.svelte
@@ -1,9 +1,9 @@
 <div class="hdr-rt">
     <div class="hdr-wrp">
         <div class="hdr-cntr">
-            <h1>
-                <pre class="html-tag">&lt;H1/&gt;</pre>Abdullah Alvan      
-            </h1>
+            <h3>
+                <pre class="html-tag">&lt;H3/&gt;</pre>Abdullah Alvan      
+            </h3>
            
             <div class="nav-blk">
                 <span>Home</span>
