a305d376a65f57193f1f75b8eb43f005589dff39
Abdullah <abdullah@gulenayva.com>
2024-09-25 17:08:45 +0300

    update url

--- myapp/src/constant/url.js
+++ myapp/src/constant/url.js
@@ -1,4 +1,5 @@
-export const isProd = true;
-export const DATA_URL = isProd === true ? "https://abdullahalvn.vercel.app/data" :  "http://localhost:3000/data"  
+export const isProd = false;
+//export const DATA_URL = isProd === true ? "https://abdullahalvn.vercel.app/data" :  "http://localhost:3000/data"  
+export const DATA_URL = "http://localhost:3000/data"
 
 
