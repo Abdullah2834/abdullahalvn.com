2f374d03ebe157bae44655645a28a2815de28772
Abdullah <abdullah@gulenayva.com>
2024-10-04 00:21:30 +0300

    update

--- myapp/src/lib/email.setup.server.js
+++ myapp/src/lib/email.setup.server.js
@@ -1,14 +1,14 @@
 //import {GOOGLE_MAIL,PASSWORD} from "$env/static/private"
 import nodemailer from "nodemailer";
-import { isProd } from "../constant/url";
+
 
 
 const transporter = nodemailer.createTransport({
-  host: isProd === true ? "live.smtp.mailtrap.io" : "sandbox.smtp.mailtrap.io" ,
+  host: "live.smtp.mailtrap.io",
   port: 587,
   auth: {
-    user: isProd === true ? "api" : "e15ec673d95cc4",
-    pass: isProd === true ? "2ec2f25bee8047fc34cb2902ecc578c8" : "b5685be57ca1cb",
+    user: "api" ,
+    pass: "2ec2f25bee8047fc34cb2902ecc578c8",
   }
 });
 
