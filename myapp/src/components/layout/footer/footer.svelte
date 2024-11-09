c985cac7239b6261092e33bd85738c05c696e5dc
Abdullah <abdullah@gulenayva.com>
2024-11-09 22:11:20 +0300

    update link & update responsive css

--- myapp/src/components/layout/footer/footer.svelte
+++ myapp/src/components/layout/footer/footer.svelte
@@ -40,19 +40,6 @@
                     <path d="M3.31912 13.4844V4.8403H0.448501V13.4844H3.31942H3.31912ZM1.88441 3.66029C2.88525 3.66029 3.50833 2.99652 3.50833 2.167C3.4896 1.31858 2.88525 0.67334 1.90344 0.67334C0.920968 0.67334 0.279297 1.31858 0.279297 2.16693C0.279297 2.99645 0.902159 3.66022 1.8656 3.66022H1.88419L1.88441 3.66029ZM4.90805 13.4844H7.77845V8.65767C7.77845 8.39966 7.79718 8.14098 7.87302 7.9567C8.08044 7.44031 8.55276 6.90577 9.34595 6.90577C10.3844 6.90577 10.8001 7.69839 10.8001 8.86055V13.4844H13.6704V8.52814C13.6704 5.87315 12.2544 4.63764 10.3658 4.63764C8.81743 4.63764 8.13739 5.50384 7.75956 6.0938H7.77867V4.8406H4.9082C4.94567 5.65152 4.90798 13.4847 4.90798 13.4847L4.90805 13.4844Z" fill="black"/>
                 </svg>                    
             </a>
-            <a href="https://www.fiverr.com/abdullahalvn?up_rollout=true" 
-               rel="noreferrer"
-               target="_blank"
-               title="Abdullah Fiverr"
-               class="scm-itm"
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#00000" width="14" height="14" viewBox="-2.5 -2 24 24" preserveAspectRatio="xMinYMin" class="jam jam-fiverr" stroke="#0000">
-                    <g id="SVGRepo_bgCarrier" stroke-width="0"/>                    
-                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-                    <g id="SVGRepo_iconCarrier">
-                    <path d="M16.25 16.25v-10h-10v-.625c0-1.034.841-1.875 1.875-1.875H10V0H8.125A5.632 5.632 0 0 0 2.5 5.625v.625H0V10h2.5v6.25H0V20h8.75v-3.75h-2.5V10h6.285v6.25H10V20h8.75v-3.75h-2.5z"/>
-                </svg>    
-            </a>
             <a href="https://github.com/Abdullah2834" 
                rel="noreferrer"
                target="_blank"
