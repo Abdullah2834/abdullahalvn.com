4068091fc2513034058020b5e9f85f5b88a10d44
Abdullah <abdullahalvan1@outlook.com>
2025-10-06 20:17:30 +0300

    update

--- myapp/src/lib/email.setup.server.js
+++ myapp/src/lib/email.setup.server.js
@@ -1,24 +1,24 @@
-//import {GOOGLE_MAIL,PASSWORD} from "$env/static/private"
-import nodemailer from "nodemailer";
+// //import {GOOGLE_MAIL,PASSWORD} from "$env/static/private"
+// import nodemailer from "nodemailer";
 
 
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "smtp.gmail.com",
-    pass: "vsllnfgwzwqlltgz",
-  }
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: "smtp.gmail.com",
+//     pass: "vsllnfgwzwqlltgz",
+//   }
+// });
 
-transporter.verify(function (error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
+// transporter.verify(function (error, success) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Server is ready to take our messages");
+//     }
+//   });
 
-  export default transporter
+//   export default transporter
